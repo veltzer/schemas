2645ddd9fc903ad8b33f476ea1877e3fd9b96888
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 02:09:18 +0300

    build fixing

---
+++ config/project.py
@@ -0,0 +1,4 @@
+project_name = "demos-d"
+project_long_description = "Demos for the d language"
+project_year_started = 2017
+project_description = """Demos for the d language"""
---
+++ config/python.py
@@ -0,0 +1,25 @@
+import config.project
+
+package_name = config.project.project_name
+
+console_scripts = []
+setup_requires = []
+run_requires = []
+test_requires = []
+dev_requires = [
+    "pymakehelper",
+    "pydmt",
+    "pyclassifiers",
+    "pycmdtools",
+    "jsonschema",
+    "JSON-Spec",
+    "yq",
+]
+install_requires = list(setup_requires)
+install_requires.extend(run_requires)
+
+extras_require = {}
+
+python_requires = ">=3.9"
+test_os = ["ubuntu-20.04"]
+test_python = ["3.9"]
