76a709eef962708ddc89fa6c11319174f37badb3
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:13 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-d"
-project_long_description = "Demos for the d language"
-project_year_started = 2017
-project_description = """Demos for the d language"""
+description_short = "Demos for the d language"
