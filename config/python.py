9ef523552418ba7a841570f40847df14c9dd8e00
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-24 03:16:47 +0300

    fixed better reqs

--- config/python.py
+++ config/python.py
@@ -3,16 +3,13 @@ from typing import List
 
 config_requires: List[str] = []
 dev_requires: List[str] = []
-install_requires: List[str] = [
-    "pycmdtools",
-    "jsonschema",
-    "JSON-Spec",
-    "yq",
-]
+install_requires: List[str] = []
 build_requires: List[str] = [
+    "pycmdtools",
     "pymakehelper",
     "pydmt",
     "check-jsonschema",
+    "yq",
 ]
 test_requires: List[str] = []
 requires = config_requires + install_requires + build_requires + test_requires
