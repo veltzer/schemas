265043ef6aafced1ba487b588f46ab3bc09ee629
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 15:55:18 +0300

    better reqs

--- config/python.py
+++ config/python.py
@@ -1,11 +1,11 @@
 """ python deps for this project """
 
-build_requires: list[str] = [
-    "pydmt",
-    "pymakehelper",
-    
-    "pycmdtools",
+import config.shared
+
+install_requires: list[str] = [
     "check-jsonschema",
     "yq",
 ]
-requires = build_requires
+build_requires: list[str] = config.shared.BUILD
+test_requires: list[str] = config.shared.TEST
+requires = install_requires + build_requires + test_requires
