bd80b069431fce4e7dde38d2561b1421a1d90c26
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-21 13:24:31 +0300

    convert config to lua

---
+++ config/project.lua
@@ -0,0 +1,9 @@
+-- project definitions
+NAME = "schemas"
+DESCRIPTION_SHORT = "Various schemas that I need web access to"
+KEYWORDS = {
+    "schema",
+    "yaml",
+    "json",
+    "xml",
+}
--- config/project.py
+++
@@ -1,9 +0,0 @@
-""" project definitions """
-name = "schemas"
-description_short = "Various schemas that I need web access to"
-keywords = [
-    "schema",
-    "yaml",
-    "json",
-    "xml",
-]
